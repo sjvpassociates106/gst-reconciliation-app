aeb3cc09589ed2a786e2f711264b6265934120d0
sjvpassociates106 <sjvpassociates106@gmail.com>
2026-03-20 15:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("GST Reconciliation (2B vs Purchase Register)")
 # FILE UPLOAD
 # ---------------------------
 file_2b = st.file_uploader("Upload GSTR-2B File", type=["xlsx"])
-file_pr = st.file_uploader("Upload Purchase Register", type=["xlsx", "csv"])
+file_pr = st.file_uploader("Upload Purchase Register", type=["xlsx","xls","csv"])
 
 # ---------------------------
 # READ FILES
